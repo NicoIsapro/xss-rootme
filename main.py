6bfe44eb7aedab995fa9310240a563cff53029a0
Nicolas Zona <nicolas.zona@epitech.eu>
2022-10-16 22:54:48 +0200

    feat(core): Add script

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+fake_items_db = []
+
+@app.get("/")
+async def root(cookies: str):
+    fake_items_db.append(cookies)
+    return {"message": "Thanks for ur cookie m8"}
+
+@app.get("/items")
+async def read_item():
+    return fake_items_db
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
