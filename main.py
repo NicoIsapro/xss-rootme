a9dcad98cff0c8865753385b0b93d9b828b792f2
Nicolas Zona <nicolas.zona@epitech.eu>
2022-10-16 23:40:03 +0200

    fix(core): change host

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ async def read_item():
     return fake_items_db
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=80, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
